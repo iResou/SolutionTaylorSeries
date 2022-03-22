5973cd932cc615ead0277a515e55cbfa17d27503
iResou <63888597+iResou@users.noreply.github.com>
2022-03-21 21:22:07 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -13,19 +13,24 @@ double Recursion(int x, int n)
     double Constante;
 
     // Condicion de terminacion
+    // Termination condition
     if (n == 0)
         return 1;
 
     // Llama la recursion AKA para calcular la Constante
+    // Call the AKA recursion to calculate the constant
     Constante = Recursion(x, n - 1);
 
     // Actualiza la potencia de X
+    // Update the power X
+
     Potencia = Potencia * x;
 
     // Factorial
     Factorial = Factorial * n;
 
     // Regresa el calculo de la seria de Taylor
+    // Return the calculation of the Taylor series
     return (Constante + Potencia / Factorial);
 }
 
@@ -51,6 +56,8 @@ int main()
     }
     //Si resolvemos la serie de Taylor mediante induccion, el resultado requerido para el valor n es de 11, sin embargo, debido a que en la primera iteracion---
     //Se llama la funcion de recursion para calcular la constante del primer termino, se le suma 1, con esto, el resultado final de n, es de 12.
+    //If we solve the Taylor series by induction, the required result for the value n is 11, however, since in the first iteration---
+    //The recursion function is called to calculate the constant of the first term, 1 is added, with this, the final result of n is 12. 
     
     return 0;
 }
