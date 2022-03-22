bb5f344853ba27e11c9ba7f00f992507a6c5b9e6
iResou <63888597+iResou@users.noreply.github.com>
2022-03-21 21:04:47 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,53 @@
+//Programa de C++ que calcula e^x utilizando recursion - Usando las series de Taylor
+//Andres Felipe Burbano Hernandez
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+
+
+using namespace std;
+
+double Recursion(int x, int n)
+{
+    static double Potencia = 1, Factorial = 1;
+    double Constante;
+
+    // Condicion de terminacion
+    if (n == 0)
+        return 1;
+
+    // Llama la recursion AKA para calcular la Constante
+    Constante = Recursion(x, n - 1);
+
+    // Actualiza la potencia de X
+    Potencia = Potencia * x;
+
+    // Factorial
+    Factorial = Factorial * n;
+
+    // Regresa el calculo de la seria de Taylor
+    return (Constante + Potencia / Factorial);
+}
+
+
+int main()
+{
+ 
+
+    int n;
+    int x;
+    int precision;
+    cout << "Ingrese el valor de x" << endl;
+    cin >> x;
+    cout << "Ingrese el valor de n" << endl;
+    cin >> n;
+    cout << "Ingrese la cantidad de numeros decimales para su visualizacion" << endl;
+    cin >> precision;
+    cout << "El valor de e^x es= " << fixed << setprecision(precision) << Recursion(x, n);
+
+    if (x == 1)
+    {
+        cout <<"\nRecuerda que el valor de n para que se cumpla el calculo de 9 decimales es: 12" << endl;
+    }
+    return 0;
+}
